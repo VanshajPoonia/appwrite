ee5666b5fda4342a5020894c803eb67e3508d708
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2021-10-30 12:55:43 +0530

    added Scroll Indicator Progress Bar

---
+++ public/scripts/ScrollIndicator.js
@@ -0,0 +1,33 @@
+import react, { useEffect, useState } from "react";
+import "./ScrollIndicator.css";
+
+const ScrollIndicator = () => {
+  const [scrollTop, setScrollTop] = react.useState(0);
+  const onScroll = () => {
+    const winScroll = document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollTop(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  return (
+    <div className="App">
+      <div className="progressMainWrapper">
+        <div
+          className="progressMainStyle"
+          style={{ width: `${scrollTop}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default ScrollIndicator;
